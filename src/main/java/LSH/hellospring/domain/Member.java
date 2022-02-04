0ba266022e14a03acaaf14a481782bbc76db400c
honeyhyuni <tjfkqwkd001@naver.com>
2022-02-04 17:03:47 +0900

    Member_클래스

--- src/main/java/LSH/hellospring/domain/Member.java
+++ src/main/java/LSH/hellospring/domain/Member.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+// 사용자 클래스
 @Entity
 public class Member {
 
